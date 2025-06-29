6d9af13dab4669620dff78b57454dae9b0048571
David Eberekpe <davideberekpe28@gmail.com>
2025-06-29 23:54:40 +0100

    Feat: Add responsive hamburger menu with toggle functionality for mobile

---
+++ headphones/8-script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.getElementById("hamburger");
+  const nav = document.getElementById("mobile-nav");
+
+  hamburger.addEventListener("click", () => {
+    nav.classList.toggle("show");
+  });
+});
